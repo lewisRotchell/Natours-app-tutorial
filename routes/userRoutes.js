2654a695f47bcf01688182397246301bfd158a16
Lewis Rotchell <rotchell92@gmail.com>
2021-02-28 14:55:57 +0000

    added update user functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,15 +10,27 @@ const {
   getUser,
   updateUser,
   deleteUser,
+  updateMe,
 } = userController;
 
-const { signup, login, forgotPassword, resetPassword } = authController;
+const {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  protect,
+} = authController;
+
+router.patch('/updateMe', protect, updateMe);
 
 router.post('/signup', signup);
 router.post('/login', login);
 
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
+
+router.patch('/updateMyPassword', protect, updatePassword);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
